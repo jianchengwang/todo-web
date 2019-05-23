d17242d4f172ad4baa841343e4f4f7fec2b36ab3
jianchengwang <jiancheng_wang@yahoo.com>
2019-05-24 00:10:49 +0800

    init functional programing code

---
+++ functional_programming/README.md
@@ -0,0 +1,4 @@
+
+functional programing code by javascript 
+
+[document](https://legacy.gitbook.com/book/mostly-adequate/mostly-adequate-guide)
---
+++ functional_programming/basic/ch2.js
@@ -0,0 +1,11 @@
+
+const hi = name => `Hi${name}`
+hi // name => `Hi ${name}`
+hi('joans') // "Hi jonas"
+
+var greeting = hi
+greeting('ben') // "Hi ben"
+
+
+
+
